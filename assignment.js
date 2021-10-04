a87388661681727b90cb76be2c6ed72322087339
akshat345 <89534010+akshat345@users.noreply.github.com>
2021-10-04 17:49:37 +0530

    Add files via upload
    These files contain some coding practice while learning javascript

---
+++ assignment.js
@@ -0,0 +1,15 @@
+/*const dataDolphin=(96=108=109)/3;
+const datakolha=(98=34=178)/3;
+if(dataDolphin>dataKolha && dataDolphin>=100)
+console.log("dataDolphin win the trophy");
+else if(dataKolha>dataDolphin && datakolha>=100)
+console.log("dataKolha win the trophy");
+else if(dataKolha>=100 && dataDolphin>=100 && dataKolha===dataDolphin)
+console.log("Bothtea, won the trophy")
+else
+console.log("noone won");
+*/
+
+
+
+//second assignment
